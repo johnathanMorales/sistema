9067ecd68f3e0d6e86e50960907465b9a60c0354
johnathanMorales <j@nolton.com.co>
2021-12-17 00:51:50 -0500

    CRUD DJANGO DEVELOTECA OK

---
+++ libreria/models.py
@@ -0,0 +1,21 @@
+
+from django.core.checks.messages import Debug
+from django.db import models
+from django.db.models.constraints import Deferrable
+# Create your models here.
+class Libro (models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100)
+    imagen = models.ImageField(upload_to='imagenes/',verbose_name="Imagen", null=True)
+    descripcion = models.TextField(verbose_name="Descripción", null=True)
+    
+    def __str__(self):
+        fila = "Título: " + self.titulo + " - " + "Descripción: " + self.descripcion
+        return fila
+    
+    def delete(self, using=None, keep_parents=False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
+    
+
+# Get an instance of a logger
